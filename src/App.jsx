d6cc6f155b44ae168da9e5d8c6e542cce87f9f4f
aatje-sy <1206993@student.roc-nijmegen.nl>
2025-05-21 16:52:15 +0200

    Index page HTML/CSS

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,30 @@ import './styling/styling.css'
 function app() {
     return (
         <>
+            <header>
+                <h1>Wasl</h1>
+                <div className="quotes-container">
+                    <p>Here will come a quote</p>
+                </div>
+                <div className={"Profile-icon x"}></div>
+            </header>
             <main>
                 <div className={"Nav-bar"}>
-                    navbar
+                    <nav>
+                        <div className="x"></div>
+                        <div className="x"></div>
+                        <div className="x"></div>
+                    </nav>
                 </div>
                 <div className={"Posts-container"}>
-                    Posts container
+                    <section className={"container"}>
+
+                    </section>
                 </div>
                 <div className="Contents-container">
+                    <div className="Content-bar">
+
+                    </div>
                     <div className="Content-bar">
 
                     </div>
