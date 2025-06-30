668cf3f821bb540db1363e02651ceb2f2ec87ddf
osama0210 <osama.asmi04@gmail.com>
2025-07-01 00:43:52 +0200

    feat(habits): added logic to add, fetch and check habits

--- src/components/HabitModal.jsx
+++ src/components/HabitModal.jsx
@@ -1,11 +1,28 @@
 import SubmitIcon from "/src/assets/submit-icon.svg"
+import React, {use, useState} from "react";
 
-export default function HabitModal({onClose}) {
+export default function HabitModal({onClose, onSubmit}) {
+    const [inputValue, setInputValue] = useState("");
+
     return (
         <div className="modal-overlay" onClick={onClose}>
             <div className="habit-modal" onClick={(e) => e.stopPropagation()}>
-                <form className="habit-form" action="">
-                    <input className="add-habit-input" type="text" placeholder="New habit..."/>
+                <form
+                    className="habit-form"
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                        if (!inputValue.trim()) return;
+                        onSubmit(inputValue);
+                        setInputValue("");
+                        onClose();
+                    }}
+                >
+                    <input className="add-habit-input"
+                           type="text"
+                           placeholder="New habit..."
+                           value={inputValue}
+                           onChange={(e) => setInputValue(e.target.value)}
+                    />
                     <button className="habit-submit-btn"><img src={SubmitIcon} alt=""/></button>
                 </form>
             </div>
--- src/components/HabitTracker.jsx
+++ src/components/HabitTracker.jsx
@@ -1,8 +1,64 @@
-import React, {useState} from "react";
-import HabitModal from './HabitModal.jsx';
+import React, {useEffect, useState} from "react";
+import {doc, addDoc, collection, getDocs, serverTimestamp, updateDoc} from "firebase/firestore";
+import HabitModal from "./HabitModal.jsx";
+import {db} from "../firebase";
+import {getAuth} from "firebase/auth";
+
 
 function HabitTracker() {
     const [showModal, setShowModal] = useState(false);
+    const [habits, setHabits] = useState([]);
+
+    async function addHabit(habitName) {
+        const auth = getAuth();
+        const user = auth.currentUser;
+
+        if (!user || !habitName.trim()) return;
+
+        try {
+            await addDoc(collection(db, "users", user.uid, "habits"), {
+                name: habitName,
+                done: false,
+                createdAt: serverTimestamp(),
+            });
+            console.log("Habit saved in Firebase")
+        } catch (err) {
+            console.error("Error saving habit:", err);
+        }
+    }
+
+    useEffect(() => {
+        const auth = getAuth();
+
+        const unsubscribe = auth.onAuthStateChanged(async (user) => {
+            if (!user) return;
+
+            const snapshot = await getDocs(collection(db, "users", user.uid, "habits"));
+            const data = snapshot.docs.map(doc => ({
+                id: doc.id,
+                ...doc.data(),
+            }));
+            setHabits(data);
+        });
+
+        return () => unsubscribe(); // opruimen
+    }, []);
+
+    async function handleToggle(habitId, currentDone) {
+        const auth = getAuth();
+        const user = auth.currentUser;
+        if (!user) return;
+
+        const habitRef = doc(db, "users", user.uid, "habits", habitId);
+        await updateDoc(habitRef, {done: !currentDone})
+
+        setHabits(prevState =>
+            prevState.map(h =>
+                h.id === habitId ? {...h, done: !currentDone} : h
+            )
+        );
+
+    }
 
     return (
         <div className="habit-page">
@@ -19,33 +75,30 @@ function HabitTracker() {
                     New Task
                 </button>
             </div>
-            {showModal && <HabitModal onClose={() => setShowModal(false)} />}
+            {showModal && (
+                <HabitModal
+                    onClose={() => setShowModal(false)}
+                    onSubmit={(addHabit)}
+                />
+            )}
 
             <div className="container habits-container">
                 <h3>Habits</h3>
                 <div className="habits-list-container">
                     <ul className="habit-list">
-                        <li className="habit-item done">
-                            <div className="checkbox-name-contaienr">
-                                <input type="checkbox"/>
-                                <span className="habit-name">Reading</span>
-                            </div>
-                            <span className="habit-count">5 🔥</span>
-                        </li>
-                        <li className="habit-item">
-                            <div className="checkbox-name-contaienr">
-                                <input type="checkbox"/>
-                                <span className="habit-name">Reading</span>
-                            </div>
-                            <span className="habit-count">5 🔥</span>
-                        </li>
-                        <li className="habit-item">
-                            <div className="checkbox-name-contaienr">
-                                <input type="checkbox"/>
-                                <span className="habit-name">Reading</span>
-                            </div>
-                            <span className="habit-count">5 🔥</span>
-                        </li>
+                        {habits.map((habit) => (
+                            <li key={habit.id} className={`habit-item ${habit.done ? "done" : ""}`}>
+                                <div className="checkbox-name-contaienr">
+                                    <input
+                                        type="checkbox"
+                                        checked={habit.done}
+                                        onChange={() => handleToggle(habit.id, habit.done)}
+                                    />
+                                    <span className="habit-name">{habit.name}</span>
+                                </div>
+                                <span className="habit-count">🔥</span>
+                            </li>
+                        ))}
                     </ul>
                 </div>
             </div>
